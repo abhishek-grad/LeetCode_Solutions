75169012437d1b4c2e2b487a14fe63fd426a1aa3
ABHISHEK SETHI <abhishek.grad.algo@gmail.com>
2026-05-10 20:35:20 +0530

    Time: 3 ms (42.61%), Space: 19.3 MB (84.15%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+
+        res = ""
+        carry =0
+
+        a = a[::-1]
+        b = b[::-1]
+
+        for i in range(max(len(a), len(b))):
+            digitA = int(a[i]) if i< len(a)  else 0
+            digitB = int(b[i]) if i< len(b)  else 0
+            
+            total = digitA + digitB + carry
+            digit = str(total % 2)
+            carry = total//2
+            res = digit + res 
+
+        if carry == 1:
+            res ="1" + res
+
+        return res
