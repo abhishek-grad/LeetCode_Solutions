7267fc4fe3fdc5eb20d193a2f3775d95e1c8b3ee
ABHISHEK SETHI <abhishek.grad.algo@gmail.com>
2026-06-09 22:52:02 +0530

    Time: 32 ms (15.2%), Space: 26.6 MB (15.2%) - LeetHub

---
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        mx, mn = -inf, inf
+
+        for n in nums:
+            mx = max(mx, n)
+            mn = min(mn, n)
+
+        return(mx - mn) * k
+        
